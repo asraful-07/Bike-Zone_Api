14cd07d9a85c9e957d1458493aa4862b769a9e98
asraful-07 <asraful0508@gmail.com>
2026-07-02 10:36:33 +0600

    feat: add Error struct for standardized API error responses

--- internal/httprespones/error.go
+++ internal/httprespones/error.go
@@ -1,7 +1,8 @@
-package htttprespones
+package httpresponses
 
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 	Details string `json:"details,omitempty"`
+	Meta    string `json:"meta,omitempty"`
 }
